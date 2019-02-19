13b05264e15e4d832262930b727d41401913ab1b
Administrator <Administrator@smart20117>
2019-02-19 16:49:38 +0900

    [박기훈,주선태] 첫

---
+++ datamodeling/src/main/java/cafe/data/datamodeling/DatamodelingApplication.java
@@ -0,0 +1,13 @@
+package cafe.data.datamodeling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamodelingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamodelingApplication.class, args);
+	}
+
+}
