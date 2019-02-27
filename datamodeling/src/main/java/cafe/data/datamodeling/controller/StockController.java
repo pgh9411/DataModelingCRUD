d630a3a3fe1ea2fc4b81e720424a13bb13a694b7
Administrator <Administrator@smart20117>
2019-02-27 11:31:23 +0900

    [박기훈,주선태] controller에 주석추가 제출 전 마지막으로 주석추가?

--- datamodeling/src/main/java/cafe/data/datamodeling/controller/StockController.java
+++ datamodeling/src/main/java/cafe/data/datamodeling/controller/StockController.java
@@ -68,8 +68,11 @@ public class StockController {
 		return "/user/userAdd";
 		//userAdd로 리턴한다
 	}
+	//post로 입력받은 userAdd를 실행하면 입력처리를 실행한다
 	@PostMapping("/userAdd")
 	public String userAdd(User user) {
+		//userAdd 메서드에 user정보를 입력 후 메서드를 실행한다
+		//index로 이동한다
 		stockservice.userAdd(user);
 		return "redirect:/index";
 	}
@@ -80,45 +83,59 @@ public class StockController {
 		return "/account/accountAdd";
 		//accountAdd로 리턴한다.
 	}
-	
+	//post로 입력받은 accountAdd(입력처리)를 실행한다
 	@PostMapping("/accountAdd")
-	public String accountAdd(HttpSession session, Account account) {
+	public String accountAdd(Account account) {
 		System.out.println("accountAdd 액션 요청");
 		stockservice.accountAdd(account);
-
+		//accountAdd메서드에 계좌정보를 입력 후 추가가 되면 계좌리스트로 이동한다
 		return "redirect:/accountList";
 	}
+	//get으로 입력받은 orderAdd(입력 폼)실행한다
 	@GetMapping("/orderAdd")
 	public String orderAdd(Account account,HttpSession session) {
 		System.out.println("orderAdd 폼 요청");
+		//orderAdd 폼이 클릭될때
+		//session 객체 accountNumber에 account의 계좌번호를 세팅한다 
+		//orderAdd 폼에 계좌번호를 자동으로 가져오기 위해 세션 이용한다
 		session.setAttribute("accountNumber", account.getAccountNumber());
 		System.out.println("accountNumber 확인");
 		return "/order/orderAdd";
 	}
+	//post로 입력받은 orderAdd(입력처리)실행한다
 	@PostMapping("/orderAdd")
 	public String orderAdd(Order order) {
 		System.out.println("orderAdd 액숀 요청");
+		//orderAdd 메서드에 order(주문정보)를 입력 메서드 실행후 orderList로 이동한다
 		stockservice.orderAdd(order);
 		return "redirect:/orderList";
 	}
+	//get으로 입력받은 accountList(계좌리스트) 실행한다
 	@GetMapping("/accountList")
 	public String accountList(HttpSession session,Model model) {
+		//userID에 세션에서 가져온 userId값을 담는다
 		String userID = (String)session.getAttribute("userId");
 		System.out.println("실행확인.....accountList");
+		//List타입으로 accuntList에 accountList메서드에 userID입력하여 리턴된 계좌리스트값을 담는다
 		List<Account> accountList = stockservice.accountList(userID);
 		System.out.println("accountList -> " + accountList);
+		//modelAccountList에 리스트 accountList값을 담는다
 		model.addAttribute("modelAccountList",accountList);
-
+		//accountList로 이동한다
 		return "/account/accountList";
 	}
+	//get으로 입력받은 orderList(주문리스트)를 실행한다
 	@GetMapping("/orderList")
 	public String orderList(HttpSession session,Model model) {
+		//userID에 세션에서 가져온 usreId값을 담는다
 		String userID = (String)session.getAttribute("userId");
 		System.out.println("실행확인.....accountList");
+		//List타입으로 orderList에 orderList메서드에 userID입력 리턴된 주문리스트값을 담는다
 		List<Order> orderList = stockservice.orderList(userID);
 		System.out.println("orderList -> " + orderList);
+		//modelOrderList에 리스트orderList 값을 담는다
 		model.addAttribute("modelOrderList",orderList);
-
+		//orderList로 이동한다
 		return "/order/orderList";
 }
 }
